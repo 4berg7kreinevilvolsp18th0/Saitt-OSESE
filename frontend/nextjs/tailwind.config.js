434325ee5c6c1a169bbb0efa851ec43f081eb434
4berg7kreinevilvolsp18th0 <4.berg7kreinevilvol.sp.18th0nd@gmail.com>
2025-12-19 03:39:59 +1000

    Update tailwind.config.js

--- frontend/nextjs/tailwind.config.js
+++ frontend/nextjs/tailwind.config.js
@@ -1,6 +1,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Включаем class-based dark mode
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
